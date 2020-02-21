67efc4e891958624046e2b8243ad8546d40b3f33
Luis Ramon da Silva Lima <57457051+ramonS2999@users.noreply.github.com>
2020-02-21 13:58:27 -0300

    Update App.java

--- UdemyExe02_06/src/app/App.java
+++ UdemyExe02_06/src/app/App.java
@@ -13,16 +13,16 @@ public class App {
         double numero = sc.nextDouble();
 
         if(numero > 0 && numero <= 25) {
-            System.out.println("[0,25]");
+            System.out.println("Intervalo [0,25]");
         }
-        else if(numero > 25 && numero <= 50) {
-            System.out.println("(25,50]");
+        else if(numero <= 50) {
+            System.out.println("Intervalo (25,50]");
         }
-        else if(numero > 50 && numero <= 75) {
-            System.out.println("(50,75]");
+        else if(numero <= 75) {
+            System.out.println("Intervalo (50,75]");
         }
-        else if(numero > 75 && numero <= 100) {
-            System.out.println("(75,100]");
+        else if(numero <= 100) {
+            System.out.println("Intervalo (75,100]");
         }
         else {
             System.out.println("Fora de intervalo");
